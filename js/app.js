8c7e57e9d9fcb2d46b88876cc0e6df0c44a4011c
Hannah Lim <hannah15198@gmail.com>
2019-04-26 18:13:55 +0700

    cleaned up css index

--- js/app.js
+++ js/app.js
@@ -1,60 +1,10 @@
 $(document).ready(function () {
-    $(".dropdown-content").hide();
-
-    var textArr = [
-        "full-stack developer",
-        "snowboarder",
-        "scuba diver",
-        "soccer player",
-        "touch rugby player"
-    ]
-
-    // loop through textArr
-    for (var i = 0; i < textArr.length; i++) {
-
-        function introText() {
-            setInterval(function () {
-                $("#intro-text").text(textArr[i]), 3000
-            })
-        }
-
-
-    }
-
+    // $(".dropdown-content").hide();
 
     $("#view-more-btn").on("click", function () {
         $(this).hide();
     })
 
-    // introTextArr = ["web-developer.", "soccer player.", "touch rugby player.", "sweet potato fan."];
-
-    // for(var i = 0; i<introTextArr.length; i++) {
-    //     introText(introTextArr[i])
-    // }
-    var introText = function () {
-        window.setTimeout(function () {
-            $("#intro-text").text("web-developer.");
-            // $("#intro-text").append("<span><img id='comp-emoji' height=45px src='../assets/images/emojis/comp.png'></span>")
-            window.setTimeout(function () {
-                $("#intro-text").text("soccer player.");
-                window.setTimeout(function () {
-                    $("#intro-text").text("touch rugby player.");
-                    window.setTimeout(function () {
-                        $("#intro-text").text("sweet potato fan.");
-                    }, 1000)
-                }, 1000)
-            }, 1000)
-        }, 2000)
-    }
-
-    introText()
-    
-    $("#intro-text").hover(function() {
-        $(".dropdown-content").show();
-        $(this).hide();
-    });
-
-    
     var fadeInTitle = function () {
         
 
@@ -71,12 +21,6 @@ $(document).ready(function () {
     }
 
     fadeInTitle();
-    // window.setTimeout(function () {
-    //     $("#intro-text").text("touch rugby player.");
-    //     window.setTimeout(function () {
-    //         $("#intro-text").text("sweet potato fan.");
-    //     }, 2000)
-    // }, 0)
 
 })
 
